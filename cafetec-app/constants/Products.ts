944a372e6a5eef2fc8251debfcbec940e9ded140
JesusBorbon <whatumean300@gmail.com>
2026-03-03 20:51:42 -0800

    UPDATE

--- cafetec-app/constants/Products.ts
+++ cafetec-app/constants/Products.ts
@@ -2,11 +2,12 @@ export const CATEGORIES = ['Todo', 'Cafetería', 'Snacks', 'Papelería', 'Bebida
 
 export const PRODUCTS = [
   // Cafetería
-  { id: '1', name: 'Latte Vainilla', price: 45, category: 'Cafetería', icon: 'cafe-outline' },
+  { id: '1', name: 'Latte Vainilla', price: 70, category: 'Cafetería', icon: 'cafe-outline' },
   { id: '2', name: 'Americano', price: 35, category: 'Cafetería', icon: 'cafe-outline' },
   { id: '3', name: 'Expreso Double', price: 40, category: 'Cafetería', icon: 'cafe-outline' },
   { id: '4', name: 'Cappuccino', price: 50, category: 'Cafetería', icon: 'cafe-outline' },
   { id: '5', name: 'Frappé Mocha', price: 60, category: 'Cafetería', icon: 'ice-cream-outline' },
+  { id: '10', name: 'MACHA', price: 60, category: 'Cafetería', icon: 'ice-cream-outline' },
 
   // Otros
   { id: '6', name: 'Cuaderno CESUN', price: 65, category: 'Papelería', icon: 'book-outline' },
